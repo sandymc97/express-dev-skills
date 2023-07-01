e10b2de0ac0f77fb886945cfbc5ba8cbbd60cbf2
Sandy <sandyahmedy7@gmail.com>
2023-06-30 20:50:17 -0500

    add partials, enable add skill, enable delete skill

--- models/skills.js
+++ models/skills.js
@@ -1,13 +1,31 @@
 const skills = [
-    {id: 125223, skill: 'Node.js', gained: true},
-    {id: 127904, skill: 'Express', gained: true},
-    {id: 139608, skill: 'Javascript', gained: true}
+    {id: 125223, skill: 'Node.js', gained: "yes"},
+    {id: 127904, skill: 'Express', gained: "yes"},
+    {id: 139608, skill: 'Javascript', gained: "yes"},
+    {id: 139609, skill: 'HTML', gained: "yes"},
+    {id: 139607, skill: 'CSS', gained: "yes"},
+    {id: 139610, skill: 'MongoDB',gained: "yes"},
   ];
 
   module.exports = {
     getAll,
-    getOne
+    getOne,
+    create,
+    deleteOne
   };
+
+  function deleteOne(id) {
+    id = parseInt(id);
+    //find the index so it knows which skill to delete
+    const idx = skills.findIndex(skill => skill.id === id);
+    skills.splice(idx, 1);
+  }
+
+  function create(skill) {
+    skill.id = Date.now() % 1000000;
+    skill.gained = true;
+    skills.push(skill);
+  }
 
   function getOne(id) {
     id = parseInt(id);
--- routes/skills.js
+++ routes/skills.js
@@ -3,9 +3,19 @@ var router = express.Router();
 var skillsCtrl= require("../controllers/skills");
 
 
-
+//GET skills (all dev skills page)
 router.get('/', skillsCtrl.index);
 
+// get /skills/new (page that has input to add skill)
+router.get("/new", skillsCtrl.new);
+
+//get /skills/:id (page when you click specific skill)
 router.get("/:id", skillsCtrl.show);
+
+//POST /skills (when you CLICK SAVE SKILL)
+router.post('/', skillsCtrl.create);
+
+//DELETE /skills/:id
+router.delete("/:id", skillsCtrl.delete);
 
 module.exports = router;
